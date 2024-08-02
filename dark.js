6c1ec4d0f1123f564804a8c71ba8b012336d3c7a
W1z4RdWP <headgenrekty@gmail.com>
2024-08-03 01:16:40 +0400

    фикс бага с дарк модом в всплывающем окне. освежил информацию

--- dark.js
+++ dark.js
@@ -1,9 +1,11 @@
 function darkMode(){
     const html = document.documentElement;
     const wasDarkMode = localStorage.getItem('darkMode') === 'true';
-  
+    const aside = document.querySelector('aside');
+
     localStorage.setItem('darkMode', !wasDarkMode)
     html.classList.toggle('dark-mode', wasDarkMode)
+    aside.classList.toggle('dark-mode', wasDarkMode)
 }
 
 function changeImage(){
